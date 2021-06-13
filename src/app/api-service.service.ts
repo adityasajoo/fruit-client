e7f897e8cfbf7502c3e67de9b4cc8753f9637dca
Aditya Sajoo <adityasajoo@gmail.com>
2021-06-13 15:09:40 +0530

    adi-update

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Observable,Subject } from 'rxjs';
+import { Observable,from } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
   img='https://m3placement.com/wp-content/uploads/2021/03/image-placeholder-350x350-1.png'
- url ="http:localhost:3000/"
+  url ="http://localhost:5000"
 
 
   constructor(private http:HttpClient) { }
 
 public fetchResults(body:any):Observable<any>{
-  return this.http.post(this.url,body)
+  let sendUrl = `${this.url}/predict?model=inceptionV3.h5`
+  return this.http.post(sendUrl,body)
 }
+
+  public welcome():Observable<any>{
+    return this.http.get(this.url);
+  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,19 +13,34 @@ succ=false
 
 
   inputChange(event:any){
+    
+
+    console.log(event.target.files[0]);
     if(event.target.files){
       var reader = new FileReader();
       reader.onload=(event:any)=>{
         this.succ=true
         this.api.img = event.target.result;
+       
+
       }
-reader.readAsDataURL(event.target.files[0])
+    reader.readAsDataURL(event.target.files[0])
+    this.api.fetchResults({file:event.target.files}).subscribe(res =>{
+      console.log(res);
+    })
+
+
     }
   }
 
 
 
   ngOnInit(): void {
+    this.api.welcome().subscribe(result=>{
+      console.log("Result ",result);
+    })
   }
+
+
 
 }
